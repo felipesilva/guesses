f3c5ae50303592c646ce9dd59f81646f1dcd0546
De Silva <204521@felipe-dasilva-7727.nyhq.nytint.com>
2012-04-20 14:35:08 -0400

    connect to the facebook

--- app.js
+++ app.js
@@ -7,40 +7,45 @@ var express = require('express'),
   routes = require('./routes'),
   http = require('http'),
   everyauth = require('everyauth'),
-  graph = require('fbgraph'),
   jade = require('jade'),
   RedisStore = require('connect-redis')(express);
 
 var app = express();
 var redis = require('redis').createClient();
 
-var options = {
-    timeout: 30000,
-    pool: { maxSockets:  Infinity },
-    headers: { connection:  "keep-alive" }
-};
-
-graph.setOptions(options);
-
+/*
 var
   FB_APP_ID = "439348696080450",
   FB_APP_SECRET = "2bd2a04474397f7531515f3326f72dd3";
+*/
+
+var 
+  FB_APP_ID = "274123879345468";
+  FB_APP_SECRET = "9172759b493d13b23c272af638259c47";
 
 everyauth.debug = true;
 
 everyauth.facebook
-    .appId(FB_APP_ID)
-    .appSecret(FB_APP_SECRET)
-    .scope('user_birthday,user_about_me')
-    .findOrCreateUser(function(session, access_token, accessTokExtra, user) {
-        session.user = user;
-        session.access_token = access_token;
-        return user;
-    })
+  .moduleTimeout(9999999999)
+  .appId(FB_APP_ID)
+  .appSecret(FB_APP_SECRET)
+  .scope('user_birthday,user_about_me')
+  .findOrCreateUser(function(session, access_token, accessTokExtra, user) {
+      session.user = user;
+      session.access_token = access_token;
+      return user;
+  })
+  .logoutPath('/logout')
+  .handleLogout(function(request, response){
+      request.session.user = null;
+      request.logout();
+      response.redirect('/');
+  })
+  .redirectPath('/');
 
 //everyauth.helpExpress(app);
 
-app.register('.html', jade);
+//app.register('.html', jade);
 
 app.configure(function(){
   //view stuff
@@ -71,6 +76,10 @@ app.configure('development', function(){
 });
 
 app.get('/', function(request, response){
+  if (!request.session.user) {
+    return response.redirect('/auth/facebook');
+  }
+
   var context = {};
 
   return response.render('index', context);
