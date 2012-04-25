9b3dd22d697b7debd91032b82e8ad70159b8b989
Felipe Silva <felipef.silva@gmail.com>
2012-04-25 12:03:51 -0400

    CCMatrix -first commit

--- specs/recommend.js
+++ specs/recommend.js
@@ -1,19 +1,19 @@
 var should = require('should');
-var recommend = require('../lib/recommend');
+var recommendify = require('../lib/recommendify');
 var matrix = require('../lib/inputMatrix');
 
-describe('Recommend', function(){
+describe('recommendify', function(){
 
 	describe('Configuration', function(){
 		it('Should return default maxNeighbors if not configured', function(){
-			var mr = new recommend.Base();
+			var mr = new recommendify.Base();
 
 			should.exist(mr.defaults.maxNeighbors);
 			mr.defaults.maxNeighbors.should.equal(50);
 		});
 
 		it('should remember max_neighbors if configured', function(){
-			var mr = new recommend.Base({
+			var mr = new recommendify.Base({
 				maxNeighbors: 23
 			});
 
@@ -22,18 +22,18 @@ describe('Recommend', function(){
 		});
 
 		it('should retrieve an input_matrix on a new instance', function(){
-			recommend.Base.inputMatrix('myinput', {similarityFunc: 'jaccard'});
-			var mr = new recommend.Base();
+			recommendify.Base.inputMatrix('myinput', {similarityFunc: 'jaccard'});
+			var mr = new recommendify.Base();
 
 			mr.INPUT_MATRICES.myinput.should.exist
 		});
 
 		it('should add an input_matrix by key', function(){
 			var input = null;
-			var mr = new recommend.Base();
+			var mr = new recommendify.Base();
 
-			recommend.Base.inputMatrix('myfirstinput', {similarityFunc: 'jaccard'});
-			recommend.Base.inputMatrix('mysecondinput', {similarityFunc: 'jaccard'});
+			recommendify.Base.inputMatrix('myfirstinput', {similarityFunc: 'jaccard'});
+			recommendify.Base.inputMatrix('mysecondinput', {similarityFunc: 'jaccard'});
 
 			for(input in mr.INPUT_MATRICES) {
 				input.should == 'myfirstinput';	
@@ -43,11 +43,28 @@ describe('Recommend', function(){
 		});
 
 		it('should retrieve an input_matrix on a new instance and intialize the correct class', function(){
-			recommend.Base.inputMatrix('myinput', {similarityFunc: 'jaccard'});
-			var mr = new recommend.Base();
+			recommendify.Base.inputMatrix('myinput', {similarityFunc: 'jaccard'});
+			var mr = new recommendify.Base();
 
 			mr.myinput.should.be.an.instanceof(matrix.JaccardInputMatrix)
 		});
+	});
+
+	describe('Process item', function(){
+
+		it('should call similarities_for on each input_matrix', function(){
+			recommendify.Base.inputMatrix('myfirstinput', {similarityFunc: 'jaccard'});
+			recommendify.Base.inputMatrix('mysecondinput', {similarityFunc: 'jaccard'});
+
+			var mr = new recommendify.Base();
+
+			//mr.myfirstinput.should_receive(:similarities_for).with('fnorditem').and_return([['fooitem',0.5]])
+      		//mr.mysecondinput.should_receive(:similarities_for).with('fnorditem').and_return([['fooitem',0.5]])
+
+      		//mr.similarity_matrix.stub!(:update)
+      		mr.processItem('fnorditem')
+		});
+
 	});
 
 });
